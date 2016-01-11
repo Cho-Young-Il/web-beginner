ea6f3b9dc5924d0b64b739f4513d961cd9f54bdf
Macintosh HD <joyikr@gmail.com>
2016-01-11 13:40:56 +0900

    sumLineNum

--- week1/test2.js
+++ week1/test2.js
@@ -1,1 +1,11 @@
-console.log('test2');
+function sumLineNum(num) {
+	var sum = 0;
+	while((num / 10) != 0) {
+		sum += num % 10;
+		num = parseInt(num / 10);
+	}
+	console.log(sum);
+}
+
+sumLineNum(54321); //15
+sumLineNum(543); //12
