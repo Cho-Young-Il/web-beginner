b1eed3543348372080102a211cecfae190c5c18a
Macintosh HD <joyikr@gmail.com>
2016-01-11 13:31:09 +0900

    makeDiamond

--- week1/test.js
+++ week1/test.js
@@ -1,1 +1,29 @@
-console.log('aa');
+function makeDiamond(num) {
+	var size = num * 2 - 1;
+	var line = 0;
+	var sIdx = 0;
+
+	while(line < size) {
+		var str = "";
+		for(var col = 1; col <= size; col++) {
+			if(col <= num + sIdx && col >= num - sIdx) {
+				str += "*";
+				continue;
+			}
+			str += " ";		
+		}
+		
+		console.log(str);
+		
+		if(line < num - 1) {
+			sIdx++;
+		} else {
+			sIdx--;
+		}
+
+		line++;
+	}
+}
+
+makeDiamond(5);
+makeDiamond(3);
