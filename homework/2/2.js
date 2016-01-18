936a745190659c4c7c57199f96aebdd659512126
Macintosh HD <joyikr@gmail.com>
2016-01-18 13:45:41 +0900

    2주차 알고리즘

--- homework/2/2.js
+++ homework/2/2.js
@@ -1,1 +1,11 @@
-11
+function reverseNum(num) {
+	var res = "역순은 : ";
+	while(num > 0) {
+		res += num % 10
+		num = parseInt(num / 10);
+	}
+	console.log(res);
+}
+
+reverseNum(54321);
+reverseNum(9876);
